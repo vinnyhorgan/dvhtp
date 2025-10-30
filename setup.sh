c1ebbf597b89449058bead75b359462ad60a5791
Daniel Vincent Horgan <vinnyhorgan@gmail.com>
2025-10-30 16:53:38 +0100

    initial sway config

--- setup.sh
+++ setup.sh
@@ -283,6 +283,164 @@ EOF
 
 ln -sfn "$HOME/.dvhtp/starship.toml" "$HOME/.config/starship.toml"
 
+# sway
+log "configuring fish shell..."
+
+cat > "$HOME/.dvhtp/sway" <<'EOF'
+set $mod Mod4
+
+set $left h
+set $down j
+set $up k
+set $right l
+
+set $term footclient
+set $menu wmenu-run -f "JetBrainsMono Nerd Font 17" -i -p "run -> "
+
+font pango:JetBrainsMono Nerd Font 14
+
+seat seat0 xcursor_theme Bibata-Modern-Classic 28
+
+input * xkb_options caps:swapescape
+
+smart_gaps on
+gaps inner 8
+gaps outer 8
+
+default_border pixel 2
+
+exec mako
+exec foot --server
+
+output * bg ~/pictures/bg.png fill
+
+input type:touchpad {
+  dwt enabled
+  tap enabled
+  natural_scroll enabled
+}
+
+input type:keyboard {
+  # currently assumes it keyboard, to fix :(
+  xkb_layout "it"
+}
+
+bindsym $mod+Return exec $term
+bindsym $mod+w kill
+bindsym $mod+space exec $menu
+
+floating_modifier $mod normal
+
+bindsym $mod+Shift+c reload
+
+bindsym $mod+Shift+e exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'
+
+bindsym $mod+$left focus left
+bindsym $mod+$down focus down
+bindsym $mod+$up focus up
+bindsym $mod+$right focus right
+
+bindsym $mod+Left focus left
+bindsym $mod+Down focus down
+bindsym $mod+Up focus up
+bindsym $mod+Right focus right
+
+bindsym $mod+Shift+$left move left
+bindsym $mod+Shift+$down move down
+bindsym $mod+Shift+$up move up
+bindsym $mod+Shift+$right move right
+
+bindsym $mod+Shift+Left move left
+bindsym $mod+Shift+Down move down
+bindsym $mod+Shift+Up move up
+bindsym $mod+Shift+Right move right
+
+bindsym $mod+1 workspace number 1
+bindsym $mod+2 workspace number 2
+bindsym $mod+3 workspace number 3
+bindsym $mod+4 workspace number 4
+bindsym $mod+5 workspace number 5
+bindsym $mod+6 workspace number 6
+bindsym $mod+7 workspace number 7
+bindsym $mod+8 workspace number 8
+bindsym $mod+9 workspace number 9
+bindsym $mod+0 workspace number 10
+
+bindsym $mod+Shift+1 move container to workspace number 1
+bindsym $mod+Shift+2 move container to workspace number 2
+bindsym $mod+Shift+3 move container to workspace number 3
+bindsym $mod+Shift+4 move container to workspace number 4
+bindsym $mod+Shift+5 move container to workspace number 5
+bindsym $mod+Shift+6 move container to workspace number 6
+bindsym $mod+Shift+7 move container to workspace number 7
+bindsym $mod+Shift+8 move container to workspace number 8
+bindsym $mod+Shift+9 move container to workspace number 9
+bindsym $mod+Shift+0 move container to workspace number 10
+
+bindsym $mod+b splith
+bindsym $mod+v splitv
+
+bindsym $mod+s layout stacking
+bindsym $mod+e layout toggle split
+
+bindsym $mod+f fullscreen
+
+bindsym $mod+Shift+space floating toggle
+
+#bindsym $mod+space focus mode_toggle
+
+bindsym $mod+a focus parent
+
+bindsym $mod+Shift+minus move scratchpad
+bindsym $mod+minus scratchpad show
+
+mode "resize" {
+  bindsym $left resize shrink width 10px
+  bindsym $down resize grow height 10px
+  bindsym $up resize shrink height 10px
+  bindsym $right resize grow width 10px
+
+  # Ditto, with arrow keys
+  bindsym Left resize shrink width 10px
+  bindsym Down resize grow height 10px
+  bindsym Up resize shrink height 10px
+  bindsym Right resize grow width 10px
+
+  # Return to default mode
+  bindsym Return mode "default"
+  bindsym Escape mode "default"
+}
+bindsym $mod+r mode "resize"
+
+# Special keys to adjust volume via PulseAudio
+bindsym --locked XF86AudioMute exec pactl set-sink-mute \@DEFAULT_SINK@ toggle
+bindsym --locked XF86AudioLowerVolume exec pactl set-sink-volume \@DEFAULT_SINK@ -5%
+bindsym --locked XF86AudioRaiseVolume exec pactl set-sink-volume \@DEFAULT_SINK@ +5%
+bindsym --locked XF86AudioMicMute exec pactl set-source-mute \@DEFAULT_SOURCE@ toggle
+# Special keys to adjust brightness via brightnessctl
+bindsym --locked XF86MonBrightnessDown exec brightnessctl set 5%-
+bindsym --locked XF86MonBrightnessUp exec brightnessctl set 5%+
+# Special key to take a screenshot with grim
+bindsym Print exec grim
+
+bar {
+  position top
+
+  status_command while date +'%d-%m-%Y %H:%M'; do sleep 60; done
+
+  colors {
+    statusline #ffffff
+    background #323232
+    inactive_workspace #32323200 #32323200 #5c5c5c
+  }
+}
+
+include /etc/sway/config.d/*
+EOF
+
+mkdir -p "$HOME/.config/sway"
+ln -sfn "$HOME/.dvhtp/sway" "$HOME/.config/sway/config"
+
 # wallpaper
 log "fetching wallpaper…"
 
